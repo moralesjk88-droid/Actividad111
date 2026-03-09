ba269217a41d39a292cd4e00549850917a949243
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 16:27:59 -0600

    ejercicio13, terminado

---
+++ Ejercicio13,11/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese el multiplicando:_");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese multiplicador:_ ");
+int b = int.Parse(Console.ReadLine());
+
+int resultado = Multiplicar(a, b);
+Console.WriteLine("La producto de la multiplicacion es:_ " + resultado);
+static int Multiplicar(int a, int b)
+{
+    return a * b;
+}
