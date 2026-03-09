5fca509c21e66d207ae046c4fd7544513c1622ab
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 14:20:29 -0600

    ejercicio2, terminado

---
+++ ejercicio2,11/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ejercicio2_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre:_ "); 
+            string nombre=Console.ReadLine().ToLower();
+            Nombre(nombre);
+         
+        }
+        static void Nombre(string nombre)
+            {
+                Console.WriteLine("Esta seguro que es su nombre? "+ nombre);
+                
+            }
+        
+    }
+}
