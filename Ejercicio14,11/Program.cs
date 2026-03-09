2e1c1e22594aa207ad11e7f4ab6aa0f035ebc98c
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 17:47:56 -0600

    Ejercicio14, terminado

---
+++ Ejercicio14,11/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese el dividendo:_");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el divisor:_ ");
+int b = int.Parse(Console.ReadLine());
+
+int resultado = Divicion(a, b);
+Console.WriteLine("El resultado es:_ " + resultado);
+static int Divicion(int a, int b)
+{
+    return a / b;
+}
