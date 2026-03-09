30a4935bf0712d6c1a2796f1049ad3def7878ab9
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 15:53:35 -0600

    ejercicio7,terminado

---
+++ Ejercicio7,11/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un número:_ ");
+int numero = int.Parse(Console.ReadLine());
+MosstrarDoble(numero);
+static void MosstrarDoble(int n)
+{
+    Console.WriteLine("El doble es:_ " + (n * 2));
+}
+
+
