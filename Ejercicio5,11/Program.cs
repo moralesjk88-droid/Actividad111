1bd39acc8af3e7f6a45f34f714f49c1486743b13
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 15:37:43 -0600

    Ejercicio5,terminado

---
+++ Ejercicio5,11/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bienvenidos a menu");
+MostrarMenu();
+
+static void MostrarMenu()
+{
+    Console.WriteLine("1. opcion1");
+    Console.WriteLine("2. opcion2");
+    Console.WriteLine("0. SALIR");
+} 
