823a3bdbe6b74dc980b7b746023f5fb18162492b
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 16:23:42 -0600

    Ejercicio12. terminado

---
+++ Ejercicio12,11/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese primer numero:_");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese segundo numero:_ ");
+int b = int.Parse(Console.ReadLine());
+
+int resultado = restar(a, b);
+Console.WriteLine("La resultado es:_ " + resultado);
+static int restar(int a, int b)
+{
+    return a - b;
+}
