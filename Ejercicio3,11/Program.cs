5c027da6712f2abb0e68054dd67dc1f625befdb8
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 15:19:24 -0600

    Ejercicio3,terminado

---
+++ Ejercicio3,11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ejercicio3_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+
+        }
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("Programacion en C#");
+
+        }
+    }
+}
