8015c1055527eba1267a06522491688f7668a951
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 13:52:05 -0600

    Ejercicicio1

---
+++ ejercicio1,11/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejercicio1_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre:_ ");
+            string nombre=Console.ReadLine();
+            Saludar(nombre);
+
+            static void Saludar(string nombre)
+            {
+                Console.WriteLine("Bienvenido " + nombre + " a mi primera funcion");
+            }
+        }
+    }
+}
