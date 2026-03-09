bf66bac76b806a51d7c696914c6c26e07c1319fb
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 16:04:15 -0600

    Ejercicio9, termiando

---
+++ Ejercicio9,11/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese primer numero:_");
+int numero1=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese segundo numero:_ ");
+int numero2=int.Parse(Console.ReadLine());
+
+SumarMostrar(numero1, numero2);
+static void SumarMostrar(int a, int b)
+{
+    Console.WriteLine("La suma de ambos es:_ "+(a+b));
+}
+
+
