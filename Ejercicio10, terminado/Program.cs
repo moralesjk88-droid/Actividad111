632cbf6d9199170afcbfa9970085af0cefd395cf
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 16:13:41 -0600

    Ejercicio10, terminado

---
+++ Ejercicio10, terminado/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese texto:_");
+string texto=Console.ReadLine();
+Console.WriteLine("Cuantas veces desea ver el texto:_ ");
+int veces=int.Parse(Console.ReadLine());
+ImprimirREpetido(texto, veces);
+static void ImprimirREpetido (string texto, int veces)
+{
+    for (int i = 0; i < veces; i++)
+    {
+        Console.WriteLine((i + 1) + ". " + texto);
+    }
+}
+
