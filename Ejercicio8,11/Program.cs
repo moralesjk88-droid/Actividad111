bc1369eb74de6e49f999677a49ff6a5bef45aaf4
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 15:58:22 -0600

    Ejercicio8, terminado

---
+++ Ejercicio8,11/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un numero:_ ");
+int numero=int.Parse(Console.ReadLine());
+MostrarCuadrado(numero);
+static void MostrarCuadrado(int n)
+{
+    Console.WriteLine("El cuadrado del numero es :_ " + (n * n));
+}
