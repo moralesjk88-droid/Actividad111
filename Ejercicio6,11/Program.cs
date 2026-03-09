15584746d25909f68e1159dd07ed8d8371135ae1
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 15:46:57 -0600

    Ejercicio6, terminado

---
+++ Ejercicio6,11/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escriba su nombre:_ ");
+string nombre = Console.ReadLine().ToUpper ();
+Saludar(nombre);
+
+static void Saludar(string nombre)
+{
+    Console.WriteLine("Hola "+nombre);
+}
