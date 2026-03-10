13d689d4f67ac6f85cf3ff117954d7af4e82cf45
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 23:01:48 -0600

    Ejercicio18, terminado

---
+++ Ejercicio18,11/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese el primer numero:_");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el tercer numero");
+int c = int.Parse(Console.ReadLine());
+
+double resultado = MenorDeTres(a, b, c);
+Console.WriteLine("El resultado es:_ " + resultado);
+static int MenorDeTres(int a, int b, int c)
+{ 
+  int menor = a;
+
+if (b < menor)
+    menor = b;
+
+if (c < menor)
+    menor = c;
+
+return menor;
+}
