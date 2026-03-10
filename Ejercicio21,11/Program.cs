27699b0aace471a9cde88f6cdde8cc2689109bf2
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 23:19:49 -0600

    Ejercicio21, terminado

---
+++ Ejercicio21,11/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un texto:_");
+string texto = Console.ReadLine();
+
+int vocales = ContarVocales(texto);
+
+Console.WriteLine("Cantidad de caracteres:_ " + vocales);
+
+static int ContarVocales(string texto)
+{
+    int contador = 0;
+    foreach (char letra in texto.ToLower())
+    {
+        if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+        {
+            contador++;
+        }
+    }
+    return contador;
+}
