8625546ab37d1a7dd0faff983ba4253d1e0b82e9
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 23:14:35 -0600

    Ejercicio20, terminado

---
+++ Ejercicio20,11/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un texto:_");
+string texto = Console.ReadLine();
+
+int cantidad=ContarLetras(texto);
+
+Console.WriteLine("Cantidad de caracteres:_ " + cantidad);
+
+static int ContarLetras(string texto)
+{
+    return texto.Length;
+}
