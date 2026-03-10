73e1d73cd31d15616fbaf64bbe19a11bc646518f
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 23:29:18 -0600

    Ejercicio23, terminado

---
+++ Ejercicio23,11/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un número:_ ");
+int n=int.Parse(Console.ReadLine());
+
+int suma=SumarDel1AlN(n);
+
+Console.WriteLine("La suma es: " + suma);
+
+static int SumarDel1AlN(int n)
+{
+    int suma = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        suma += i;  
+    }
+    return suma;
+
+}
