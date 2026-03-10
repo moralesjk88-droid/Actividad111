845ce3f65380d8958c3e40bcabd36691963b16fe
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 22:26:10 -0600

    Ejercicio15,terminado

---
+++ Ejercicio15,11/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un numero positivo:_");
+int nunero= int.Parse(Console.ReadLine());
+
+bool resultado = EsPar(nunero);
+
+Console.WriteLine("El resultado es:_ " + resultado);
+static bool EsPar(int a)
+{
+    return a % 2 == 0;
+}
