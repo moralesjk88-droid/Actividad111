d9b8a41d51d13852c117a9579d106e45afa56935
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 23:07:41 -0600

    Ejercicio19, terminado

---
+++ Ejercicio19,11/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un numero positivo:_");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese un numero positivo:_");
+int m = int.Parse(Console.ReadLine());
+
+bool resultado = EsMultiplo(n,m);
+
+Console.WriteLine("¿Es múltiplo?: " + resultado);
+static bool EsMultiplo(int n, int m)
+{
+    return n%m == 0;
+}
