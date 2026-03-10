266b08e8fc2e9e87c3e9996ad0558bb867062cc8
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 22:51:59 -0600

    Ejercicio17, terminado

---
+++ Ejercicio17,11/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese el primer numero:_");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero");
+int b = int.Parse(Console.ReadLine());
+
+double resultado = Mayor(a, b);
+Console.WriteLine("El resultado es:_ " + resultado);
+static int Mayor(int a, int b)
+{
+    if (a>b)
+    {
+        return a; 
+    }
+    else 
+    {
+        return b;
+    }
+   
+}
