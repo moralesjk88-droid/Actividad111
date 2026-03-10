423a51834d84ff960589ff56aa675223931c35da
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 23:33:38 -0600

    Ejercicio24, terminado

---
+++ Ejercicio24,11/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un número");
+int n=int.Parse(Console.ReadLine());
+
+int resultado=Factorial(n);
+
+Console.WriteLine("El factorial es:_" + resultado);
+
+static int Factorial(int n)
+{
+    int factorial = 1;
+    for(int i = 0; i < n; i++)
+    { 
+        factorial *= i; 
+    }
+    return factorial;
+}
