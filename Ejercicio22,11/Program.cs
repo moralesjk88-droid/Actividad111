f93ea953b8e325bf63b2e0c728fa5a5eb3bba49b
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 23:24:41 -0600

    Ejercicio22, terminado

---
+++ Ejercicio22,11/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese texto:_");
+string texto = Console.ReadLine();
+Console.WriteLine("Cuantas veces desea ver el texto:_ ");
+int veces = int.Parse(Console.ReadLine());
+string resultado=Repetido(texto, veces);
+Console.WriteLine(resultado);
+static string Repetido(string texto, int veces)
+{
+    string resultado = " ";
+    for (int i = 0; i < veces; i++)
+    {
+        resultado += texto + "\n";
+    }
+    return resultado;
+}
