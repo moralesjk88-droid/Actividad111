c1cf967fdcc30d26d105596b92b8fdf3975a69ba
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-09 23:39:01 -0600

    Ejercicio25, terminado

---
+++ Ejercicio25.11/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un número:_");
+int n = int.Parse(Console.ReadLine());
+
+bool primo =EsPrimo(n);
+
+Console.WriteLine("¿Es primmo?: " + primo);
+
+static bool EsPrimo(int n)
+{
+    if (n<=1)
+    {
+        return false;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
